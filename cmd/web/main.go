4f79d031392a0d4c3950cc14a535a8cb7481b054
The-good-guyy <80090032+The-good-guyy@users.noreply.github.com>
2024-08-03 23:58:26 +0700

    commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,34 @@
 package main
 import (
+"flag"
 "log"
 "net/http"
+"os"
 )
 func main() {
-mux := http.NewServeMux()
-mux.HandleFunc("/", home)
-mux.HandleFunc("/snippet/view", snippetView)
-mux.HandleFunc("/snippet/create", snippetCreate)
-log.Print("Starting server on :4000")
-err := http.ListenAndServe(":4000", mux)
-log.Fatal(err)
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+	f, err := os.OpenFile("tmp/info.log", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	infoLog := log.New(f, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
