0822930876f3305fbf189ce8d096ffb7fa0f2e3f
The-good-guyy <80090032+The-good-guyy@users.noreply.github.com>
2024-09-13 19:19:08 +0700

    commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,12 @@
 package main
 import (
+"database/sql"
 "flag"
 "log"
 "net/http"
 "os"
+
+ _ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -13,32 +16,39 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
-	f, err := os.OpenFile("tmp/info.log", os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	infoLog := log.New(f, "INFO\t", log.Ldate|log.Ltime)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db,err := openDB(*dsn)
+	if err!=nil{
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 	app:= &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
 	}
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler: app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+func openDB(dsn string)(*sql.DB,error){
+	db,err:= sql.Open("mysql",dsn)
+	if err!=nil{
+		return nil,err
+	}
+	if err:= db.Ping();err!=nil{
+		return nil,err
+	}
+	return db,nil
 }
